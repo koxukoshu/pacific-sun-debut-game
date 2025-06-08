995b350f4c429124825ce47ad194c3586c781485
ashdsabandal <adsabandal@up.edu.ph>
2025-06-09 04:37:44 +0800

    feat: add button click sound effect

--- new_game_project_2025-06-06_13-04-02/control.gd
+++ new_game_project_2025-06-06_13-04-02/control.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var button_click: AudioStreamPlayer = $ButtonClick
 
 func _on_start_pressed() -> void:
+	button_click.play()
+	var timer = get_tree().create_timer(0.4)
+	await timer.timeout 
 	get_tree().change_scene_to_file("res://game.tscn")
