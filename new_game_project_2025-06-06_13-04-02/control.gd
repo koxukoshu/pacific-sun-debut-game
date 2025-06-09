a0ed80544cf4a3c9c725bc2cefc026c2dce8e0dd
ashdsabandal <adsabandal@up.edu.ph>
2025-06-09 10:44:58 +0800

    feat: add credits

--- new_game_project_2025-06-06_13-04-02/control.gd
+++ new_game_project_2025-06-06_13-04-02/control.gd
@@ -1,9 +1,21 @@
 extends Control
 
 @onready var button_click: AudioStreamPlayer = $ButtonClick
+@onready var credits_2: Sprite2D = $credits2
 
+func _ready() -> void:
+	credits_2.visible = false
+	
 func _on_start_pressed() -> void:
 	button_click.play()
 	var timer = get_tree().create_timer(0.4)
 	await timer.timeout 
 	get_tree().change_scene_to_file("res://game.tscn")
+	
+
+func _on_credits_pressed() -> void:
+	button_click.play()
+	var timer = get_tree().create_timer(0.4)
+	await timer.timeout 
+	
+	credits_2.visible = !credits_2.visible
